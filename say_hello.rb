b604444ee9218a846a218e63b279165d5ef86649
Devin Wilson <craigvin84@yahoo.com>
2020-08-17 21:17:30 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,9 +1,6 @@
 # Build your say_hello method here
-def say_hello('name')
-if name == "Kent Beck"
-  puts "Hello Kent Beck!"
-else name != "Kent Beck"
-  puts "Ruby Programmer!"
+def say_hello(name="Ruby Programmer")
+  puts "Hello #{name}!"
 end
 
   say_hello
